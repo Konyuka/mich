6792bf6d2c82d4caf817956f40e29af2487eb3e2
Michael Saiba <46402493+Konyuka@users.noreply.github.com>
2021-07-12 10:33:47 +0300

    adding client

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -440,8 +440,9 @@
       </div>
     </div>
 
-    <!-- This example requires Tailwind CSS v2.0+ -->
-    <div class="py-12 bg-white">
+    
+    <!-- More services Section -->
+    <!-- <div class="py-12 bg-white">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="lg:text-center">
           <h2
@@ -465,7 +466,6 @@
                 <div
                   class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-900 text-white"
                 >
-                  <!-- Heroicon name: outline/globe-alt -->
                   <svg
                     class="h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
@@ -498,7 +498,6 @@
                 <div
                   class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-900 text-white"
                 >
-                  <!-- Heroicon name: outline/scale -->
                   <svg
                     class="h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
@@ -531,7 +530,6 @@
                 <div
                   class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-900 text-white"
                 >
-                  <!-- Heroicon name: outline/lightning-bolt -->
                   <svg
                     class="h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
@@ -564,7 +562,6 @@
                 <div
                   class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-900 text-white"
                 >
-                  <!-- Heroicon name: outline/annotation -->
                   <svg
                     class="h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
@@ -593,9 +590,12 @@
             </div>
           </dl>
         </div>
+        
       </div>
-    </div>
+    </div> -->
 
+    
+    
     <!-- This example requires Tailwind CSS v2.0+ -->
     <div class="servicesBG">
       <div class="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
@@ -606,7 +606,7 @@
             <h2
               class="text-3xl font-extrabold text-white tracking-tight sm:text-4xl text-center"
             >
-              Meet our team
+              Clients
             </h2>
           </div>
           <ul
@@ -617,214 +617,77 @@
             >
               <div class="space-y-6 xl:space-y-10">
                 <img
-                  class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
-                  src="https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixqx=XiDO1s76Hq&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+                  class="mx-auto h-40 w-40 rounded-sm xl:w-56 xl:h-56"
+                  src="@/assets/img/nrb.jpeg"
                   alt=""
                 />
                 <div
                   class="space-y-2 xl:flex xl:items-center xl:justify-between"
                 >
                   <div class="font-medium text-lg leading-6 space-y-1">
-                    <h3 class="text-red-900">Julian</h3>
-                    <p class="text-black text-sm">Managing Director</p>
+                    <p class="text-black text-sm">The Nairobi Hospital</p>
                   </div>
-
-                  <ul class="flex justify-center space-x-5">
-                    <li>
-                      <a href="#" class="text-gray-400 hover:text-gray-300">
-                        <span class="sr-only">Twitter</span>
-                        <svg
-                          class="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          aria-hidden="true"
-                        >
-                          <path
-                            d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" class="text-gray-400 hover:text-gray-300">
-                        <span class="sr-only">LinkedIn</span>
-                        <svg
-                          class="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          aria-hidden="true"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                  </ul>
+                  
                 </div>
               </div>
             </li>
+
+            <li
+              class="py-10 px-6 bg-black text-center rounded-lg xl:px-10 xl:text-left shadow-2xl"
+            >
+              <div class="space-y-6 xl:space-y-10">
+                <img
+                  class="mx-auto h-40 w-40 rounded-sm xl:w-56 xl:h-56"
+                  src="@/assets/img/vip.jpeg"
+                  alt=""
+                />
+                <div
+                  class="space-y-2 xl:flex xl:items-center xl:justify-between"
+                >
+                  <div class="font-medium text-lg leading-6 space-y-1">
+                    <p class="text-white text-sm">The VIP Hotel, Madeya</p>
+                  </div>
+                  
+                </div>
+              </div>
+            </li>
+
             <li
               class="py-10 px-6 bg-white text-center rounded-lg xl:px-10 xl:text-left shadow-2xl"
             >
               <div class="space-y-6 xl:space-y-10">
                 <img
-                  class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
-                  src="https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixqx=XiDO1s76Hq&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+                  class="mx-auto h-60 w-40 rounded-sm  xl:w-66 xl:h-56"
+                  src="https://landportal.org/sites/landportal.org/files/styles/width280/public/37.jpg?itok=iyjHKDDc"
                   alt=""
                 />
                 <div
                   class="space-y-2 xl:flex xl:items-center xl:justify-between"
                 >
                   <div class="font-medium text-lg leading-6 space-y-1">
-                    <h3 class="text-red-900">Charles</h3>
-                    <p class="text-black text-sm">Media Relations</p>
+                    <p class="text-black text-sm">Ministry of Lands & Pyhiscal Planning</p>
                   </div>
-
-                  <ul class="flex justify-center space-x-5">
-                    <li>
-                      <a href="#" class="text-gray-400 hover:text-gray-300">
-                        <span class="sr-only">Twitter</span>
-                        <svg
-                          class="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          aria-hidden="true"
-                        >
-                          <path
-                            d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" class="text-gray-400 hover:text-gray-300">
-                        <span class="sr-only">LinkedIn</span>
-                        <svg
-                          class="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          aria-hidden="true"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                  </ul>
+                  
                 </div>
               </div>
             </li>
+
             <li
-              class="py-10 px-6 bg-white text-center rounded-lg xl:px-10 xl:text-left shadow-2xl"
+              class="py-10 px-6 bg-black text-center rounded-lg xl:px-10 xl:text-left shadow-2xl"
             >
               <div class="space-y-6 xl:space-y-10">
                 <img
-                  class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
-                  src="https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixqx=XiDO1s76Hq&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+                  class="mx-auto h-40 w-40 rounded-sm xl:w-56 xl:h-56"
+                  src="@/assets/img/inuka.jpeg"
                   alt=""
                 />
                 <div
                   class="space-y-2 xl:flex xl:items-center xl:justify-between"
                 >
                   <div class="font-medium text-lg leading-6 space-y-1">
-                    <h3 class="text-red-900">Waithera</h3>
-                    <p class="text-black text-sm">Digital Marketing</p>
+                    <p class="text-white text-sm">Inuka Hospital</p>
                   </div>
-
-                  <ul class="flex justify-center space-x-5">
-                    <li>
-                      <a href="#" class="text-gray-400 hover:text-gray-300">
-                        <span class="sr-only">Twitter</span>
-                        <svg
-                          class="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          aria-hidden="true"
-                        >
-                          <path
-                            d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" class="text-gray-400 hover:text-gray-300">
-                        <span class="sr-only">LinkedIn</span>
-                        <svg
-                          class="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          aria-hidden="true"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </li>
-            <li
-              class="py-10 px-6 bg-white text-center rounded-lg xl:px-10 xl:text-left shadow-2xl"
-            >
-              <div class="space-y-6 xl:space-y-10">
-                <img
-                  class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
-                  src="https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixqx=XiDO1s76Hq&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
-                  alt=""
-                />
-                <div
-                  class="space-y-2 xl:flex xl:items-center xl:justify-between"
-                >
-                  <div class="font-medium text-lg leading-6 space-y-1">
-                    <h3 class="text-red-900">Laureen</h3>
-                    <p class="text-black text-sm">Content Marketing</p>
-                  </div>
-
-                  <ul class="flex justify-center space-x-5">
-                    <li>
-                      <a href="#" class="text-gray-400 hover:text-gray-300">
-                        <span class="sr-only">Twitter</span>
-                        <svg
-                          class="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          aria-hidden="true"
-                        >
-                          <path
-                            d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" class="text-gray-400 hover:text-gray-300">
-                        <span class="sr-only">LinkedIn</span>
-                        <svg
-                          class="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          aria-hidden="true"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                  </ul>
+                  
                 </div>
               </div>
             </li>
